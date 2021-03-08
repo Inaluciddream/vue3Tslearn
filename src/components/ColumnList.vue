0d7a2f4b8e75208bbd3250c60acb44f1252d6b89
LAPTOP-8FDU68DF\lucid <499340357@qq.com>
2021-03-08 16:59:54 +0800

    first commit

---
+++ src/components/ColumnList.vue
@@ -0,0 +1,33 @@
+<template>
+    <div>
+
+    </div>
+</template>
+
+<script lang="ts">
+import { defineComponent, PropType } from 'vue'
+export interface ColumnProps {
+    id: number;
+    title: string;
+    avatar: string;
+    description: string;
+}
+export default defineComponent({
+    name: 'CloumnList',
+    props: {
+        list: {
+            type: Array as PropType<ColumnProps[]>,
+            required: true
+        }
+    },
+    setup (props) {
+        
+        
+        return {}
+    }
+})
+</script>
+
+<style scoped>
+
+</style>
