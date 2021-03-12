47a865347f5e681c8793cfe705d8d65629dd8ff3
LAPTOP-8FDU68DF\lucid <499340357@qq.com>
2021-03-12 20:54:31 +0800

    抽离Dropdown 里的 点击隐藏dropdown逻辑为hooks

--- src/components/Dropdown.vue
+++ src/components/Dropdown.vue
@@ -1,5 +1,5 @@
 <template>
-    <div class="dropdown">
+    <div class="dropdown" ref="DropElement">
         <a href="#" class="btn btn-outline-light my-2 dropdown-toggle" @click.prevent="toggleOpen">{{title}}</a>
         <ul class="dropdown-menu" v-if="isOpen" style="display: block;">
             <slot></slot>
@@ -8,8 +8,8 @@
 </template>
 
 <script lang="ts">
-import { defineComponent, ref } from 'vue'
-
+import { defineComponent, ref, watch } from 'vue'
+import useClickOutside from '../hooks/useClickOutside'
 export default defineComponent({
     name: 'Dropdown',
     props: {
@@ -19,13 +19,21 @@ export default defineComponent({
         }
     },
     setup () {
+        const DropElement = ref<null | HTMLElement>(null)
         const isOpen = ref(false)
         const toggleOpen = () => {
             isOpen.value = !isOpen.value
         }
+        const isClickOutside = useClickOutside(DropElement)
+        watch(isClickOutside, () => {
+            if (isClickOutside.value && isOpen.value) {
+                isOpen.value = false
+            }
+        })
         return {
             isOpen,
-            toggleOpen
+            toggleOpen,
+            DropElement
         }
     }
 })
